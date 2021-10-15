5ed59aeabd9ea40aab166bfca83cc2c7faf8b5c7
lmmar-ic <skf$gRGH%%746>
2021-10-15 09:08:58 +0200

    First print model for client

---
+++ repair_product_print_models/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Repair Product Print Models",
+    "summary": "Repair products Print for client, for Count Product, and for labels pieces.",
+    "version": "14.0.1.0.0",
+    "category": "Repair",
+    "author": "Pedro Guirao, ",
+    "website": "https://github.com/OCA/account-analytic",
+    "license": "AGPL-3",
+    "depends": ["repair","repair_product"],
+    "data": [
+        "views/report_repairorder_extended.xml"
+    ],
+    "installable": True,
+}
