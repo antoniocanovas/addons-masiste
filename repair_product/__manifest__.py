38bb03e12e30cb6bc97fc0cb23509ffe64584d5f
lmmar-ic <skf$gRGH%%746>
2021-10-15 08:49:30 +0200

    1 modelo impresión terminado

--- repair_product/__manifest__.py
+++ repair_product/__manifest__.py
@@ -16,8 +16,7 @@
         "views/repair_view.xml",
         "views/menu_views.xml",
         "views/company_view.xml",
-        "views/repair_for_client_report_document.xml",
-        "views/menu_report_views.xml"
+        "views/report_repairorder_extended.xml"
     ],
     "installable": True,
 }
