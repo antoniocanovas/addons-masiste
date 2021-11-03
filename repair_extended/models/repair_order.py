710d04dbd15700312665c699a04c5058bb166b2b
pedroguirao <pedro@serincloud.com>
2021-11-03 10:55:31 +0100

    repair_extended

--- repair_extended/models/repair_order.py
+++ repair_extended/models/repair_order.py
@@ -15,17 +15,33 @@ class RepairOrder(models.Model):
     @api.depends('quotation_line_ids')
     def get_quotation_subtotal(self):
         for record in self:
+            sub = 0
             for line in record.quotation_line_ids:
-                sub = line.price_subtotal
+                sub += line.price_subtotal
             record.quotation_price_subtotal = sub
     quotation_price_subtotal = fields.Monetary(string="Subtotal", compute="get_quotation_subtotal", store=True)
 
     @api.depends('quotation_line_ids')
     def get_quotation_total(self):
         for record in self:
+            tot = 0
             for line in record.quotation_line_ids:
-                tot = line.price_total
+                tot += line.price_total
             record.quotation_price_total = tot
-
     quotation_price_total = fields.Monetary(string="Total", compute="get_quotation_total", store=True)
+
+    @api.depends('quotation_line_ids')
+    def get_quotation_taxes(self):
+        for record in self:
+            tax = 0
+            for line in record.quotation_line_ids:
+                taxes = line.tax_ids.compute_all(line.price_unit, quantity=line.product_qty,
+                                                   product=line.product_id,
+                                                   partner=False)
+                price_tax = sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
+                tax += price_tax
+            record.quotation_taxes = tax
+
+    quotation_taxes = fields.Monetary(string="Tax", compute="get_quotation_taxes", store=True)
+
     print_quotation_lines = fields.Boolean(string="Print QL")
--- repair_extended/models/repair_quotation_line.py
+++ repair_extended/models/repair_quotation_line.py
@@ -11,10 +11,23 @@ class RepairQuotationLine(models.Model):
 
     repair_id = fields.Many2one('repair.order', string='Repair Order')
     product_id = fields.Many2one("product.product", string="Product")
-    product_uom_id = fields.Many2one("uom.uom", string="Uom")
-
     product_qty = fields.Float(string="Product qty", default="1")
     currency_id = fields.Many2one('res.currency', string='Currency', readonly=1, related=False)
+
+    @api.depends('product_id')
+    def get_default_uom(self):
+        for record in self:
+            record.product_uom_id = record.product_id.uom_id
+    product_uom_id = fields.Many2one("uom.uom", string="Uom", compute="get_default_uom")
+
+    @api.depends('product_id')
+    def get_default_description(self):
+        for record in self:
+            record.name = record.product_id.name
+    name = fields.Char(string="Description",
+                       compute=get_default_description,
+                       readonly=False,
+                       store=True)
 
     @api.depends('product_qty','product_uom_id','product_id')
     def get_unit_price(self):
@@ -28,21 +41,23 @@ class RepairQuotationLine(models.Model):
                     else:
                         p['price_unit'] = p.product_id.lst_price
 
-    price_unit = fields.Monetary(string="Unit price", compute="get_unit_price", store=True)
+    price_unit = fields.Monetary(string="Unit price", compute="get_unit_price", store=True , readonly=False,)
 
     tax_ids = fields.Many2many("account.tax", string="Taxes", related="product_id.taxes_id", readonly=1)
 
     @api.depends('product_qty', 'product_uom_id', 'product_id')
     def get_subtotal_price(self):
-        self.price_subtotal = self.price_unit * self.product_qty
+        for record in self:
+            record.price_subtotal = record.price_unit * record.product_qty
     price_subtotal = fields.Monetary(string="Subtotal", compute="get_subtotal_price", store=True)
 
     @api.depends('product_qty', 'product_uom_id', 'product_id')
     def get_total_price(self):
-        taxes = self.tax_ids.compute_all(self.price_unit, quantity=self.product_qty, product=self.product_id,
-                                        partner=False)
-        price_tax = sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
-        self.price_total = self.price_unit + price_tax
+        for record in self:
+            taxes = record.tax_ids.compute_all(record.price_unit, quantity=record.product_qty, product=record.product_id,
+                                            partner=False)
+            price_tax = sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
+            record.price_total = record.price_subtotal + price_tax
 
     price_total = fields.Monetary(string="Total", compute="get_total_price", store=True)
 
