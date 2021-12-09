19501820959cab207dda731315cb2de9fb3c6656
Antonio Cánovas <antonio.canovas@ingenieriacloud.com>
2021-12-09 10:05:01 +0100

    Add print reports mods
    Sale.order y account.move changes en presupuestos y facturas

---
+++ reports_masiste/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Reports without taxes ",
+    "summary": "Reports from sale.order and account wihtout taxes.",
+    "version": "14.0.1.0.0",
+    "category": "Reports",
+    "author": "Pedro Guirao, ",
+    "website": "https://github.com/OCA/account-analytic",
+    "license": "AGPL-3",
+    "depends": [],
+    "data": [
+        "views/report_sale_order_without_taxes.xml",
+        "views/report_account_move_without_taxes.xml",
+    ],
+    "installable": True,
+}
