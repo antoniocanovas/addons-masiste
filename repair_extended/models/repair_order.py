e919f28d4419be029a4a519318f67983249e7c9d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-02-10 01:34:31 +0100

    account information

--- repair_extended/models/repair_order.py
+++ repair_extended/models/repair_order.py
@@ -11,6 +11,17 @@ class RepairOrder(models.Model):
     quotation_line_ids = fields.One2many('repair.quotation.line', 'repair_id', string="Quotation line")
     date_out = fields.Datetime('Date out')
     signature = fields.Binary('Signature')
+    partner_credit = fields.Monetary(related='partner_id.credit', string='Credit')
+    payment_term_id = fields.Many2one('account.payment.term', string='Payment term', store="False",
+                                      related='partner_id.property_payment_term_id')
+
+    @api.depends('partner_id')
+    def get_pending_invoices(self):
+        for record in self:
+            invoices = self.env['account.move'].search([('move_type', '=', 'out_invoice'),
+                                                        ('payment_state', '=', ['not_paid', 'in_payment'])])
+            record['pending_invoice_ids'] = [(6, 0, invoices.ids)]
+    pending_invoice_ids = fields.Many2many("account.move", string="Invoices", store=False, compute="get_pending_invoices")
 
     @api.depends('quotation_line_ids')
     def get_quotation_subtotal(self):
