bb171700885063dd4a6eef489147d4462941b399
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-05-30 16:45:09 +0200

    custom_masiste

---
+++ custom_masiste/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Custom Masiste",
+    "summary": "",
+    "version": "14.0.1.0.0",
+    "category": "Customization",
+    "author": "Pedro Guirao, ",
+    "website": "",
+    "license": "AGPL-3",
+    "depends": ['crm',],
+    "data": [
+        "views/crm_lead_view.xml",
+    ],
+    "installable": True,
+}
