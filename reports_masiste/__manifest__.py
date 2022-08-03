dfc1b302baf763f5d4fe24f4e279c9f34a3777a3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-08-03 10:24:25 +0200

    reports 2.0

--- reports_masiste/__manifest__.py
+++ reports_masiste/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Reports without taxes ",
     "summary": "Reports from sale.order and account wihtout taxes.",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "category": "Reports",
     "author": "Pedro Guirao, ",
     "website": "https://github.com/OCA/account-analytic",
