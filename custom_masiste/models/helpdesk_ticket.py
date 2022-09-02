ac0e3fde21faa596e719bae04d0f5d75b9b2efc3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-09-02 10:35:37 +0200

    upd helpdesk

---
+++ custom_masiste/models/helpdesk_ticket.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2021 Serincloud S.L. All Rights Reserved
+#    PedroGuirao pedro@serincloud.com
+##############################################################################
+from odoo import fields, models, api
+
+class HelpdeskTicket(models.Model):
+    _inherit = "helpdesk.ticket"
+
+    delegacion_id = fields.Many2one('res.partner', string='Delegación')
+    provincia_id = fields.Many2one('res.country.state', string='Provincia',
+                                   related='delegacion_id.state_id', store=True)
