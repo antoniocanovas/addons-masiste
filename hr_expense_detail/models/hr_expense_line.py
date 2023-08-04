483e27c78efb01b245eae78973a39ba8a138f4ce
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-04 11:55:45 +0200

    bug hr_expense_line

--- hr_expense_detail/models/hr_expense_line.py
+++ hr_expense_detail/models/hr_expense_line.py
@@ -19,7 +19,7 @@ class HrExpeseLine(models.Model):
         for record in self:
             record.name = record.type_id.name
 #    name = fields.Char('Description', compute='get_name_from_type', readonly=False)
-    name = fields.Char('Description', default='type_id.name', readonly=False)
+    name = fields.Char('Description', default=type_id.name, readonly=False)
 
     @api.depends('type_id')
     def get_standard_amount(self):
