6a99bbe0f6cc7b95dba1d182cfd8ef8b80b9ec49
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-21 23:56:31 +0100

    req1

--- custom_masiste/models/__init__.py
+++ custom_masiste/models/__init__.py
@@ -2,3 +2,4 @@ from . import crm_lead
 from . import sale_order
 from . import helpdesk_ticket
 from . import project_project
+from . import res_partner
---
+++ custom_masiste/models/res_partner.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2021 Serincloud S.L. All Rights Reserved
+#    PedroGuirao pedro@serincloud.com
+##############################################################################
+from odoo import api, fields, models, _
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    maps = fields.Char('Maps', store=True)
